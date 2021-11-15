a702a4c8f1653f18d18055e33f3a6fdd5c810cb1
Carolina Gomez <carolinagomezTX@gmail.com>
2021-11-15 08:59:57 -0600

    logging func

--- src/app.js
+++ src/app.js
@@ -15,4 +15,12 @@ const sayHello = (req, res, next) => {
     res.send("Hello");
 }
 
+const logging = (req,res,next) => {
+    console.log("A request is being made!");
+    next();
+}
+
+app.use(logging);
+app.use(sayHello);
+
 module.exports = app;
