f8eb820fdd07b46cfef2bc353c790c2fc68c50b0
Carolina Gomez <carolinagomezTX@gmail.com>
2021-11-15 08:56:13 -0600

    sayHello middleware on app.js

--- src/app.js
+++ src/app.js
@@ -6,4 +6,13 @@
 const express = require("express");
 const app = express();
 
+/**
+ * Middleware
+ * A  function that an Express server runs between
+ * receiving a request and responding to that request.
+ */
+const sayHello = (req, res, next) => {
+    res.send("Hello");
+}
+
 module.exports = app;
