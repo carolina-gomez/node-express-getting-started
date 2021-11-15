eafd1f7c9438367f4cfa2192eb4d9c50b690d70e
Carolina Gomez <carolinagomezTX@gmail.com>
2021-11-15 06:50:27 -0600

    sever.js

--- src/server.js
+++ src/server.js
@@ -2,3 +2,9 @@
  * This file is where you will run your
  * Express application.
  */
+
+const { PORT = 3001 } = process.env;
+const app = require("./app");
+
+const listener = () => console.log(`Listening on Port ${PORT}!`);
+app.listen(PORT, listener);
